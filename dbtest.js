4a69232d4a5c0eb7537a62cef5890546999480e8
pekko1215 <s16t218@stu.kagawa-u.ac.jp>
2018-03-21 23:18:37 +0900

    dwa

---
+++ dbtest.js
@@ -0,0 +1,16 @@
+const couchdb = require('node-couchdb');
+
+const client = new couchdb({
+	host:'couchdb.cloudno.de',
+	protocol:'https',
+	port:5984,
+	auth:{
+		user:'zelard1215',
+		pass:'fIG1iCIlsI'
+	}
+})
+client.listDatabases().then((dbs)=>{
+	console.log(dbs)
+}).catch((err)=>{
+	console.error(err)
+})
--- server.js
+++ server.js
@@ -1,35 +1,48 @@
 const express = require('express');
-const couchdb = require('couchdb');
+const couchdb = require('node-couchdb');
 
 const app = express.createServer();
 
-const client = couchdb.createClient(5984,'couchdb.cloudno.de','pekko12152','j9Hct6fra0');
-
-const db = client.db('musetest');
-
-app.get('/',(req,res)=>{
-	var docName = req.param('docName');
-	var author = req.param('author');
-
-	if(docName && author){
-		db.saveDoc({docName,author},(err,ok)=>{
-			if(err){
-				res.json({
-					error:true,
-					message:JSON.stringify(err)
-				})
-				return
-			}
-			res.json({error:false})
-		});
+const client = new couchdb({
+	host:'couchdb.cloudno.de',
+	protocol:'https',
+	port:5984,
+	auth:{
+		user:'zelard1215',
+		pass:'fIG1iCIlsI'
 	}
-	db.allDocs({
-		include_docs:true
-	},(err,resp)=>{
-		var docList = resp.rows;
-		res.json(rows);
-	})
 })
 
-app.listen(SERVER_PORT);
-console.log("Server on port %s", app.address().port);
+client.listDatabases().then((dbs)=>{
+	console.log(dbs)
+}).catch((err)=>{
+	console.error(err)
+})
+// const db = client.db('musetest');
+
+// app.get('/',(req,res)=>{
+//	var docName = req.param('docName');
+//	var author = req.param('author');
+
+//	if(docName && author){
+//		db.saveDoc({docName,author},(err,ok)=>{
+//			if(err){
+//				res.json({
+//					error:true,
+//					message:JSON.stringify(err)
+//				})
+//				return
+//			}
+//			res.json({error:false})
+//		});
+//	}
+//	db.allDocs({
+//		include_docs:true
+//	},(err,resp)=>{
+//		var docList = resp.rows;
+//		res.json(rows);
+//	})
+// })
+
+// app.listen(SERVER_PORT);
+// console.log("Server on port %s", app.address().port);
